64dcfd52926ef2fdf2f4f3c07d68baa54caf78ec
tharking <kamal.choudhary@gmail.com>
2014-11-24 01:16:02 +0530

    added plot3 files

---
+++ plot3.R
@@ -0,0 +1,16 @@
+## read data
+NEI <- readRDS("./summarySCC_PM25.rds")
+## turn year into a factor
+mydata <- transform(NEI, year = factor(year))
+mydata <- transform(mydata, type = factor(type))
+##filter data
+datafiltered <- mydata[mydata$fips == "24510",]
+## It assumes user has already installed plyr
+library(plyr)
+# sum by emissions for every year & type
+plotdata <- ddply(datafiltered, .(year, type), summarize, sum = sum(Emissions))
+#create multifacet ggplot chart
+library(ggplot2)
+png("plot3.png")
+ggplot(plotdata, aes(year, sum)) + facet_grid(. ~ type) + geom_boxplot(aes(fill=type)) + labs(x="YEAR", y="Total Emissions") + guides(fill=F)
+dev.off()
